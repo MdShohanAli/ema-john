edd2ca3c4b60e1889d64ecfa5c48eee63541f4a6
MdShohanAli <720shohan@gmail.com>
2022-09-28 15:15:03 +0600

    complite navbar

---
+++ src/Component/Shop/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Hello I am From Shop</h2>
+        </div>
+    );
+};
+
+export default Shop; <h2>Hello I am From Shop</h2>
